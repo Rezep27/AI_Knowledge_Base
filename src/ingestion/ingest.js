87029856ee49eda4412b043e280403522c596a58
Jairo <jairospm2002@gmail.com>
2026-08-10 13:01:45 -0500

    Finalized ingestion and chunk creation

--- src/ingestion/ingest.js
+++ src/ingestion/ingest.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 import {
-    SentenceSplitter
+    TokenTextSplitter
 } from "llamaindex";
 import { PDFReader } from "@llamaindex/readers/pdf";
 
@@ -15,9 +15,9 @@ export async function ingestPDF(filePath) {
 
     console.log(`Documents loaded: ${documents.length}`);
 
-    const splitter = new SentenceSplitter({
-        chunkSize: 512,
-        chunkOverlap: 50
+    const splitter = new TokenTextSplitter({
+        chunkSize: 256,
+        chunkOverlap: 40
     });
 
     const nodes = splitter.getNodesFromDocuments(
@@ -26,11 +26,27 @@ export async function ingestPDF(filePath) {
 
     console.log(`Nodes created: ${nodes.length}`);
 
-    for (const [index, node] of nodes.slice(115, 120).entries()) {
+    let fPath = filePath.split("/");
+    let nameSplit = fPath[fPath.length - 1].split(".");
+    let name = nameSplit[0];
+    let newNodes = []
+    for (let i = 1; i <= nodes.length; i++) {
+        let node = nodes[i - 1];
+        newNodes.push({
+            id: `${name}-${i}`,
+            content: node.getContent(),
+            metadata: node.metadata
+        });
+    }
+
+    for (const [index, node] of newNodes.slice(115, 120).entries()) {
         console.log("\n==============================");
         console.log(`NODE ${index}`);
         console.log("==============================");
-        console.log(node.getContent());
+        console.log("ID:");
+        console.log(node.id);
+        console.log("CONTENT:");
+        console.log(node.content);
         console.log("METADATA:");
         console.dir(node.metadata);
     }
